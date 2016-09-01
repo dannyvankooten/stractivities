aa44401bf9b59c061879b8b2ed907a7dc7a9275f
Danny van Kooten <dannyvankooten@gmail.com>
2016-09-01 12:53:58 +0200

    Allow shift-click to select multiple elements at once.

--- public/assets/browserify/views/activities.js
+++ public/assets/browserify/views/activities.js
@@ -1,5 +1,34 @@
 'use strict'
 
+var lastChecked;
+
+function selectMultiple(e) {
+  if( e.shiftKey && lastChecked ) {
+    var elements = document.querySelectorAll('input[name="'+ e.target.name +'"]');
+    var inRange = false;
+
+    // loop through similar elements on page
+    for( var i=0; i<elements.length; i++) {
+
+      // if in range, keep copying checked attribute
+      if( inRange ) {
+        elements[i].checked = e.target.checked;
+
+        // move out of range if at first or last element
+        if( elements[i].value === lastChecked.value || elements[i].value === e.target.value) {
+          inRange = false;
+        }
+      } else if(elements[i].value === lastChecked.value || elements[i].value === e.target.value) {
+        // if not,  keep checking if we're in range yet
+        inRange = true;
+      }
+    }
+  }
+
+  // store reference to last checked element
+  lastChecked = e.target;
+}
+
 module.exports = function(m, activities) {
   return m("table", [
     // table header
@@ -19,7 +48,12 @@ module.exports = function(m, activities) {
         var date = new Date(a.start_date_local);
 
         return m("tr", [
-          m('td', m("input", { type: "checkbox", name: "activities[]", value: a.id, checked: i < 6 ? true : false })),
+          m('td', m("input", {
+            type: "checkbox",
+            name: "activities[]",
+            value: a.id,
+            onclick: selectMultiple,
+          })),
           m("td.meta", date.toDateString()),
           m("td.strong", a.name),
           m("td.centered", Math.round(a.elapsed_time/60) + " min"),
